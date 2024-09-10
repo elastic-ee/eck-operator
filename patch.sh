9dd5dfa1ca9ce7dbf7a314f520cf2bace1bae8ee
Công Trần <trancong12102@gmail.com>
2024-09-10 16:03:58 +0700

    chore: update patch commands order

--- patch.sh
+++ patch.sh
@@ -13,24 +13,20 @@ VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 
 git checkout "$VERSION"
 git pull origin "$VERSION"
-
-SHA1=$(git rev-parse --short=8 --verify HEAD)
-
-GO_LDFLAGS="-X github.com/elastic/cloud-on-k8s/v2/pkg/about.version=$VERSION \
-	-X github.com/elastic/cloud-on-k8s/v2/pkg/about.buildHash=$SHA1 \
-	-X github.com/elastic/cloud-on-k8s/v2/pkg/about.buildDate=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
-  -X github.com/elastic/cloud-on-k8s/v2/pkg/about.buildSnapshot=false"
-
 git apply ../license.patch
 
-
+SHA1=$(git rev-parse --short=8 --verify HEAD)
+GO_LDFLAGS="-X github.com/elastic/cloud-on-k8s/v2/pkg/about.version=$VERSION \
+  -X github.com/elastic/cloud-on-k8s/v2/pkg/about.buildHash=$SHA1 \
+  -X github.com/elastic/cloud-on-k8s/v2/pkg/about.buildDate=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
+  -X github.com/elastic/cloud-on-k8s/v2/pkg/about.buildSnapshot=false"
 OPERATOR_IMAGE=ghcr.io/trancong12102/eck-operator:$VERSION
 
 docker buildx build . \
 	 	-f build/Dockerfile \
-		--build-arg GO_LDFLAGS='$GO_LDFLAGS' \
+		--build-arg GO_LDFLAGS="$GO_LDFLAGS" \
 		--build-arg GO_TAGS='' \
-		--build-arg VERSION='$VERSION' \
+		--build-arg VERSION="$VERSION" \
 		--push \
 		-t $OPERATOR_IMAGE
 
